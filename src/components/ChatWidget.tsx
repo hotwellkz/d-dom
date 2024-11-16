9ef3d27d2e2d08a9450538f388c227f3d9385007
hotwellkz <a777mmm@mail.ru>
2024-11-16 23:00:02 +0600

    509

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -139,7 +139,7 @@ export default function ChatWidget() {
   };
 
   return (
-    <div className="fixed bottom-4 right-4 z-50">
+    <div className="fixed bottom-[92px] md:bottom-4 right-4 z-50">
       <AnimatePresence>
         {isOpen && (
           <motion.div
