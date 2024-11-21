2a9261a02844ddcda6773393ec34c7249486f3ea
hotwellkz <hotwell.kz@gmail.com>
2024-11-21 19:36:54 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['react', 'react-dom', 'react-router-dom', 'firebase/app', 'firebase/firestore', 'firebase/auth'],
           ui: ['lucide-react']
         }
       }
