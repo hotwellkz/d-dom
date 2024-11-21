ac0dff51cf546adda7ff4383daeb35479a1e0a62
hotwellkz <hotwell.kz@gmail.com>
2024-11-21 20:34:32 +0600

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,4 +3,17 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDagc1X
+  apiKey: "AIzaSyDagc1XcgKNcLpKs7ISKHDq0zuUfN7b7eo",
+  authDomain: "hotwellkz-accounting.firebaseapp.com", 
+  projectId: "hotwellkz-accounting",
+  storageBucket: "hotwellkz-accounting.appspot.com",
+  messagingSenderId: "218067666070",
+  appId: "1:218067666070:web:ad244a97f338aa2e5f2eb7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize services
+export const db = getFirestore(app);
+export const auth = getAuth(app);
